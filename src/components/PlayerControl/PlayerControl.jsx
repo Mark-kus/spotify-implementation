fbe815d3371773172cca8ebe6a19a4ae322d0cf5
Marco <tignanellimarco@gmail.com>
2023-07-11 18:57:21 -0300

    player progress bar

--- src/components/PlayerControl/PlayerControl.jsx
+++ src/components/PlayerControl/PlayerControl.jsx
@@ -1,7 +1,7 @@
 import './PlayerControl.css'
 import { useState } from "react"
 
-export default function PlayerControl({resource}) {
+export default function PlayerControl({ resource }) {
 
     // Important: Some tracks doesn't have preview_url, so they won't play
     const [currentIndex, setCurrentIndex] = useState(0)
@@ -17,12 +17,12 @@ export default function PlayerControl({resource}) {
 
     const nextTrack = () => {
         if (currentIndex >= tracks.length) return
-        
+
         audio.pause()
         const track = tracks[currentIndex + 1]
 
         setCurrentIndex((currentIndex) => currentIndex + 1)
-        if (track.preview_url) setAudio(new Audio(track.preview_url))
+        if (track?.preview_url) setAudio(new Audio(track.preview_url))
         if (!isPlaying) setIsPlaying(true)
     }
 
@@ -33,14 +33,23 @@ export default function PlayerControl({resource}) {
         const track = tracks[currentIndex - 1]
 
         setCurrentIndex((currentIndex) => currentIndex - 1)
-        if (track.preview_url) setAudio(new Audio(track.preview_url))
+        if (track?.preview_url) setAudio(new Audio(track.preview_url))
         if (!isPlaying) setIsPlaying(true)
     }
+
+    setInterval(() => {
+        // {audio.ended && currentIndex < tracks.length ? nextTrack() : null}
+        setTrackProgress((audio.currentTime / audio.duration) * 100)
+    }, 1000);
 
     { isPlaying ? audio.play() : audio.pause() }
 
     return (
         <>
+            <div className='progressBar'>
+                <div className='backProgressBar'>-</div>
+                <div className='frontProgressBar' style={{ width: `${trackProgress}%` }} >-</div>
+            </div>
             <button onClick={previousTrack}>Previous</button>
             <button onClick={playAudio} >Play</button>
             <button onClick={pauseAudio} >Pause</button>
