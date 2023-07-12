d893df98444c2917472f0f10729fbae3954e0f9b
Marco <tignanellimarco@gmail.com>
2023-07-12 14:03:30 -0300

    add full functionality to progress bar

--- src/components/PlayerControl/PlayerControl.jsx
+++ src/components/PlayerControl/PlayerControl.jsx
@@ -1,21 +1,30 @@
 import './PlayerControl.css'
-import { useState } from "react"
-import MusicBar from '../MusicBar/MusicBar'
+import { useRef, useState } from "react"
 
 export default function PlayerControl({ tracks }) {
 
     // Important: Some tracks doesn't have preview_url, so they won't play
+    const [trackProgress, setTrackProgress] = useState(0)
     const [currentIndex, setCurrentIndex] = useState(0)
     const [isPlaying, setIsPlaying] = useState(false)
-
     const [music, setMusic] = useState(new Audio(tracks[0].preview_url))
+    const intervalId = useRef(null)
+
+    const createBarInterval = (audio) => {
+        return setInterval(() => {
+            const progress = (audio.currentTime / audio.duration) * 100
+            setTrackProgress(progress)
+        }, 50);
+    }
 
     const toggleMusicPlay = () => {
         if (isPlaying) {
             music.pause()
+            clearInterval(intervalId.current)
             setIsPlaying(false)
         } else {
             music.play()
+            intervalId.current = createBarInterval(music)
             setIsPlaying(true)
         }
     }
@@ -25,12 +34,16 @@ export default function PlayerControl({ tracks }) {
         setCurrentIndex((currentIndex) => currentIndex + 1)
 
         music.pause()
+        clearInterval(intervalId.current)
+
         const track = tracks[currentIndex + 1]
         const nextMusic = new Audio(track.preview_url)
 
         if (track?.preview_url) {
             nextMusic.play()
             setMusic(nextMusic)
+            setTrackProgress(0)
+            intervalId.current = createBarInterval(nextMusic)
             if (!isPlaying) setIsPlaying(true)
         }
     }
@@ -40,19 +53,27 @@ export default function PlayerControl({ tracks }) {
         setCurrentIndex((currentIndex) => currentIndex - 1)
 
         music.pause()
+        clearInterval(intervalId.current)
+
         const track = tracks[currentIndex - 1]
         const previousMusic = new Audio(track.preview_url)
 
         if (track?.preview_url) {
             previousMusic.play()
             setMusic(previousMusic)
+            setTrackProgress(0)
+            intervalId.current = createBarInterval(previousMusic)
             if (!isPlaying) setIsPlaying(true)
         }
     }
 
+
     return (
         <>
-            <MusicBar music={music} />
+            <div className='progressBar'>
+                <div className='backProgressBar'>-</div>
+                <div className='frontProgressBar' style={{ width: `${trackProgress}%` }} >-</div>
+            </div>
             <div className='music-controls'>
                 <button onClick={previousTrack}>{"<<"}</button>
                 <button onClick={toggleMusicPlay}>{isPlaying ? "||" : ">"}</button>
