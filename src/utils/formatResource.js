66379fd6371f1ece74d05a0e6b3f534f38eff9c7
Marco <tignanellimarco@gmail.com>
2023-07-12 16:43:12 -0300

    give a correct format to tracks and users

--- src/utils/formatResource.js
+++ src/utils/formatResource.js
@@ -1,5 +1,5 @@
 export default function formatResource(data, searchType) {
-
+    // Returned resource format
     const formatedResource = {
         name: null,
         owner: null,
@@ -10,28 +10,43 @@ export default function formatResource(data, searchType) {
     // Each type gives us the data in different ways
     switch (searchType) {
         case "tracks":
+            // The track property just holds the preview_url
+            const formatedTrack = {
+                name: null,
+                preview_url: data.preview_url
+            }
+
             formatedResource.name = data.name
             formatedResource.owner = data.album.artists[0].name
             formatedResource.image = data.album.images[0].url
+            formatedResource.tracks = [formatedTrack]
+
             return formatedResource
 
         case "playlists":
+            // The tracks property holds each name and preview_url
+            const formatedTracks = data.tracks.items.map(track => {
+                return {
+                    name: track.track.name,
+                    preview_url: track.track.preview_url
+                }
+            })
+
             formatedResource.name = data.name
             formatedResource.owner = data.owner.display_name
             formatedResource.image = data.images[0].url
-            formatedResource.tracks = [...data.tracks.items]
-            return formatedResource
+            formatedResource.tracks = [...formatedTracks]
 
-        case "shows":
-            formatedResource.name = data.name
-            formatedResource.owner = data.publisher
-            formatedResource.image = data.images[0].url
             return formatedResource
 
         case "users":
+            // The tracks property holds an empty object
+            // If there is no image, gives a default cdn
             formatedResource.name = data.display_name
             formatedResource.owner = data.publisher
-            formatedResource.image = "https://i.scdn.co/image/ab6761610000e5eb55d39ab9c21d506aa52f7021"
+            formatedResource.image = data.images[data.images.length - 1].url || "https://i.scdn.co/image/ab6761610000e5eb55d39ab9c21d506aa52f7021"
+            formatedResource.tracks = [{ name: null, preview_url: null }]
+
             return formatedResource
 
         default:
