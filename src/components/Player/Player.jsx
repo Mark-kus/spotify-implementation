8d6a219817e280816c2c58446e1c5ab4170a4043
Marco <tignanellimarco@gmail.com>
2023-07-14 20:07:01 -0300

    fix margins

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -9,8 +9,8 @@ export default function Player({ resource }) {
             <div className="resource-card">
                 <img src={resource.image} alt="Imagen de portada del recurso" width={156} className="resource-logo" />
                 <div className="top-card">
-                    <a className="link-to" target="_blank" href={resource.url}><h2>{resource.name}</h2></a>
-                    {isNaN(Number(resource.owner)) ? <p className="mute">{resource.owner}</p> : <FollowButton href={resource.url} qty={resource.owner} />}
+                    <a className="link-to" target="_blank" href={resource.url}><h2 className="margin-no">{resource.name}</h2></a>
+                    {isNaN(Number(resource.owner)) ? <p className="mute margin-no">{resource.owner}</p> : <FollowButton href={resource.url} qty={resource.owner} />}
                     <div className="genres">
                         {resource.genres.map((genre, i) => {
                             const shownGenre = genre.slice(0, 1).toUpperCase() + genre.slice(1)
